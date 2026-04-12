2f7c6574b348b0ee96053a461f1c593d8a2049a7
gagik77 <gagi-2008@mail.ru>
2026-04-12 18:55:29 +0300

    .

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
-from fastapi import FastAPI, Request
-from fastapi.templating import Jinja2Templates
-from contextlib import asynccontextmanager
-from database import setup_database
+from fastapi import FastAPI,Request
 from routers import music
+from fastapi.templating import Jinja2Templates
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    await setup_database()
-    print(" База данных готова")
-    yield
-
-    print("Приложение остановлено")
-
-app = FastAPI(lifespan=lifespan)
+app=FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-app.include_router(music.router, prefix='/music')
+app.include_router(music.router,prefix='/music')
+
 
 @app.get('/')
 def index(request: Request):
